d734023934ab34b502be71f6e77a5f4aeec251d3
sridhar goudu <sridhargoudu7@gmail.com>
2025-12-30 14:59:50 +0530

    Time: 0 ms (100.00%) | Memory: 24.9 MB (19.62%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,18 +1,44 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        left = 0 
-        right = len(nums) - 1
+        # left = 0 
+        # right = len(nums) - 1
 
-        while left < right:
+        # while left < right:
+        #     mid = (left + right) // 2
+
+        #     # base intension is pair is starts at even until one odd value find .....
+        #     if mid% 2 == 1:
+        #         mid -= 1
+            
+        #     if nums[mid] == nums[mid + 1]:
+        #         left = mid +2
+        #     else:
+        #         right = mid
+        
+        # return nums[left]
+
+
+
+    
+        n = len(nums)
+    
+        if len(nums) == 1: return nums[0]
+        if nums[0] != nums[1] : return nums[0]
+        if nums[n-1] != nums[n-2] : return nums[n-1]
+         
+        left = 0
+        right = n-2 
+        
+        while left <= right:
             mid = (left + right) // 2
 
-            # base intension is pair is starts at even until one odd value find .....
-            if mid% 2 == 1:
-                mid -= 1
+            if nums[mid] != nums[mid-1] and nums[mid] != nums[mid+1]:
+                return nums[mid]
             
-            if nums[mid] == nums[mid + 1]:
-                left = mid +2
+            if (mid%2 == 0 and nums[mid] == nums[mid+1]) or (mid % 2  and nums[mid] == nums[mid-1]):
+                left = mid + 1
             else:
-                right = mid
-        
-        return nums[left]
+                right = mid - 1
+
+        return -1 
+
