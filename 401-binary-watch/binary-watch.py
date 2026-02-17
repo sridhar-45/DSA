82d81295fdd986eab80f4c02cf4feb4b3ddcf098
sridhar goudu <sridhargoudu7@gmail.com>
2026-02-17 08:54:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (13.49%) - LeetSync

--- 401-binary-watch/binary-watch.py
+++ 401-binary-watch/binary-watch.py
@@ -1,11 +1,21 @@
-class Solution:
-    def readBinaryWatch(self, turnedOn: int) -> List[str]:
-        res = []
+# class Solution:
+#     def readBinaryWatch(self, turnedOn: int) -> List[str]:
+#         res = []
 
-        for hh in range(12):
-            for mm in range(60):
-                if (bin(hh).count("1") + bin(mm).count("1")) == turnedOn:
-                    minute = f"{mm:02d}"   # add leading zero if needed
-                    res.append(f"{hh}:{minute}")
+#         for hh in range(12):
+#             for mm in range(60):
+#                 if (bin(hh).count("1") + bin(mm).count("1")) == turnedOn:
+#                     minute = f"{mm:02d}"   # add leading zero if needed
+#                     res.append(f"{hh}:{minute}")
         
-        return res
+#         return res
+
+
+class Solution:
+    def readBinaryWatch(self, turnedOn: int):
+        return [
+            f"{h}:{m:02d}"
+            for h in range(12)
+            for m in range(60)
+            if (h.bit_count() + m.bit_count()) == turnedOn
+        ]
