00ef8d52bf934707da3bcdd46b53d988db2f0501
sridhar goudu <sridhargoudu7@gmail.com>
2026-03-04 16:56:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (85.11%) - LeetSync

---
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numSpecial(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        m = len(mat[0])
+        rows = [0]* n
+        cols = [0] * m
+
+        for i in range(n):
+            for j in range(m):
+                if mat[i][j] == 1:
+                    rows[i] += 1
+                    cols[j] += 1
+
+        cnt = 0
+        for i in range(n):
+            for j in range(m):
+                
+                if mat[i][j] == 1 and rows[i]- 1 == 0 and cols[j] -1 == 0:
+                    cnt += 1
+
+        return cnt            
+
