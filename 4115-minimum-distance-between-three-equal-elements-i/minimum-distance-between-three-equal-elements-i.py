824ee20bc85cb2550c71915c3203a2a92d09bf2b
sridhar goudu <sridhargoudu7@gmail.com>
2026-04-10 15:14:06 +0530

    Time: 3 ms (67.11%) | Memory: 19.3 MB (48.66%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
@@ -1,14 +1,25 @@
+from collections import defaultdict
 class Solution:
     def minimumDistance(self, nums: List[int]) -> int:
         n = len(nums)
         if n < 3 :
             return -1
         
-        ans = inf
-        for i in range(n):
-            for j in range(i+1, n):
-                for k in range(j+1, n):
-                    if nums[i] == nums[j] == nums[k]:
-                        ans = min(ans , abs(i - j) + abs(j - k) + abs(k - i))
+        # ans = inf
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         for k in range(j+1, n):
+        #             if nums[i] == nums[j] == nums[k]:
+        #                 ans = min(ans , abs(i - j) + abs(j - k) + abs(k - i))
 
-        return ans if ans != inf else -1
+        # return ans if ans != inf else -1
+
+        mpp = defaultdict(list)
+        ans = inf
+        for ind, num in enumerate(nums):
+            if len(mpp[num]) >= 2:
+                ans = min(ans, ind - mpp[num][-2])
+            mpp[num].append(ind)
+        
+        return ans * 2 if ans != inf else -1
+            
