78b3851fea21354d5836f470bac7d6c18087a885
sridhar goudu <sridhargoudu7@gmail.com>
2026-05-10 23:49:51 +0530

    Time: 390 ms (81.05%) | Memory: 19.3 MB (86.93%) - LeetSync

---
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximumJumps(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+
+        dp = [-1] * n
+
+        # base case
+        dp[0] = 0
+
+        for i in range(n):
+
+            # unreachable index
+            if dp[i] == -1:
+                continue
+
+            for j in range(i + 1, n):
+
+                diff = nums[j] - nums[i]
+
+                if -target <= diff <= target:
+
+                    dp[j] = max(dp[j], dp[i] + 1)
+
+        return dp[-1]
